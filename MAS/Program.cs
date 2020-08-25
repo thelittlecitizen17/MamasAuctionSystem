f65623028091a8710b3e339137c95f749d996724
Yakov Pizenberg <thelittlecitizen17@gmail.com>
2020-08-25 19:59:32 +0300

    Creating new project

---
+++ MAS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
